6a1fd306ecfcc8b460e70e4e37d5c1fd15b65a61
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-20 19:04:31 +0200

    wip

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -2,7 +2,10 @@
 import * as yup from 'yup'
 
 const formSchema = yup.object().shape({
-  username: yup,
+  username: yup.string()
+    .trim()
+    .min(3, 'The username must be at least three characters long')
+    .required('The username is a required field'),
   email: yup,
   // role: yup,
   // civil: yup,
