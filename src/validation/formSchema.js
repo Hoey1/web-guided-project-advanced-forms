cb8cce9b62d93516c5fd702ee12309c494c8bfa1
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-20 19:02:11 +0200

    wip

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,6 +1,11 @@
 // Here goes the schema for the form
 import * as yup from 'yup'
 
-const formSchema = {}
+const formSchema = yup.object().shape({
+  username: yup,
+  email: yup,
+  // role: yup,
+  // civil: yup,
+})
 
 export default formSchema
